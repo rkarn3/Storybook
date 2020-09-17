74f0a299dec0f08c06209efb1f4ebb71458b4ab9
Roshan Kumar Karn <rkkarn1@gmail.com>
2020-09-17 16:38:03 +0530

    some edits

--- app.js
+++ app.js
@@ -1,89 +1,104 @@
-const path = require('path')
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const connectDB = require('./config/db')
-
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const connectDB = require('./config/db');
 
 //load config
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
 
 //passport config
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 //bodyparser
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //method override
-app.use(methodOverride(function (req, res) {
+app.use(
+  methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
-      let method = req.body._method
-      delete req.body._method
-      return method
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
     }
-  }))
+  })
+);
 
 //logging
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 //HAndlebars helpers
-const { formatDate,stripTags, truncate,editIcon,select } = require('./helpers/hbs')
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs');
 
 //handlebars
-app.engine('.hbs',exphbs({helpers:{
-    formatDate,
-    stripTags,
-    truncate,
-    editIcon,
-    select,
+app.engine(
+  '.hbs',
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
     },
-    defaultLayout: 'main', extname: '.hbs'}))
-    app.set('view engine','.hbs')
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+);
+app.set('view engine', '.hbs');
 
-//session 
-app.use(session({
+//session
+app.use(
+  session({
     secret: 'Rosan',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection})
-}))
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 //passport midddlewares
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Set global variable 
+// Set global variable
 app.use(function (req, res, next) {
-    res.locals.user = req.user || null
-    next()
-  })
-  
+  res.locals.user = req.user || null;
+  next();
+});
 
 //static folder
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
-app.use('/',require('./routes/index'))
-app.use('/auth',require('./routes/auth'))
-app.use('/stories',require('./routes/stories'))
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, ()=>{
-    console.log(`Server Started On PORT ${PORT} and running in ${process.env.NODE_ENV}`);
-})
+app.listen(PORT, () => {
+  console.log(
+    `Server Started On PORT ${PORT} and running in ${process.env.NODE_ENV}`
+  );
+});
--- config/db.js
+++ config/db.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = async ()=>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopoplogy: true,
-            useFindAndModify: false
-        })
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopoplogy: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
 
-        console.log(`MongoDB COnnected: ${conn.connection.host}`)
-    } catch (err){
-        console.error(err)
-        process.exit(1)
-    }
-}
+    console.log(`MongoDB COnnected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
